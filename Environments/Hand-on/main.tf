c4887a82bb561c9b5dc59b60f75bd4eedc26f12a
ankoay <ankoayfeno@gmail.com>
2025-03-31 20:21:49 +0000

    done

--- Environments/Hand-on/main.tf
+++ Environments/Hand-on/main.tf
@@ -4,5 +4,8 @@ module "vpc" {
 
 module "ec2" {
   source            = "../../Modules/ec2"
+  ami               = "ami-0779caf41f9ba54f0"
+  instance_type     = "t2.micro"
+  instance_name     = "ec2-demo"
   security_groups   = ["allow-ports",]
 }
--- Modules/ec2/main.tf
+++ Modules/ec2/main.tf
@@ -4,14 +4,15 @@ resource "aws_key_pair" "my_key" {
 }
 
 resource "aws_instance" "ec2_demo" {
-  ami             = "ami-0779caf41f9ba54f0"
-  instance_type   = "t2.micro"
+  ami             = var.ami
+  instance_type   = var.instance_type
+
   security_groups = var.security_groups
   key_name        = aws_key_pair.my_key.key_name
 
   
   tags = {
-    Name = "ec2-demo"
+    Name = var.instance_name
   }
   #user_data = file("${path.module}/cloud-init.yaml")
 
@@ -27,7 +28,7 @@ resource "null_resource" "force_provision" {
 
   provisioner "local-exec" {
     command = <<EOT
-      echo "[ec2-demo]" > "${path.root}/Environments/Hand-on/ansible/hosts.ini"
+      echo "[${aws_instance.ec2_demo.tags.Name}]" > "${path.root}/Environments/Hand-on/ansible/hosts.ini"
       echo "${aws_instance.ec2_demo.public_ip} ansible_user=admin ansible_ssh_private_key_file=${path.root}/Key/key" >> "${path.root}/Environments/Hand-on/ansible/hosts.ini"
       ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
         -i '${path.root}/Environments/Hand-on/ansible/hosts.ini' \
